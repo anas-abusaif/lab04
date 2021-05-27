69accb98dfd7140ded356de854ef1321956cd4a3
AnasAbusief <>
2021-05-27 13:52:07 +0000

    added green website

---
+++ app.js
@@ -0,0 +1,9 @@
+confirm("you are now entering the green website");
+var username=prompt("please inform us with your name");
+alert("welcome "  + username );
+ 
+ var answer=prompt("do you like eating fruits and vegitables?");
+
+ if( answer === 'yes') {document.write("<p>keep doing the right thing</p>")}
+ else if( answer === 'no'){document.write("you should reconcider your life choices")};
+
